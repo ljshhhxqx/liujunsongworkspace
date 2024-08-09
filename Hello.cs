45163df3eafe5f710c00a5d36176428d5d067e5d
DESKTOP-DU1KDKN\LxJqS <sserljs@qq.com>
2024-08-09 10:11:49 +0800

    重命名

--- Hello.cs
+++ Hello.cs
@@ -1,24 +1,20 @@
 using UnityEngine;
 
-namespace HotUpdate
+public class Hello 
 {
-    public class Hello 
+    public static void SayHello()
     {
-        public static void SayHello()
-        {
-            Debug.Log("Hello from HotUpdate!");
-        }
+        Debug.Log("Hello from HotUpdate!");
+    }
 
-        public static void TestType<T>()
-        {
-            var obj = default(T);
-            if (obj != null) Debug.Log(obj.GetType());
-        }
+    public static void TestType<T>(T obj)
+    {
+        if (obj != null) Debug.Log(obj.GetType());
+    }
 
-        public static void TestAddComponent()
-        {
-            var go = new GameObject("Test");
-            go.AddComponent<TestComponent>();
-        }
+    public static void TestAddComponent()
+    {
+        var go = new GameObject("Test");
+        go.AddComponent<TestComponent>();
     }
 }
--- TestComponent.cs
+++ TestComponent.cs
@@ -7,7 +7,7 @@ public class TestComponent : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("TestComponent Start");
     }
 
     // Update is called once per frame
