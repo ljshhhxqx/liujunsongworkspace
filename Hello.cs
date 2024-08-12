01df8455aa8905eca258c8db7764be0b48794b05
DESKTOP-DU1KDKN\LxJqS <sserljs@qq.com>
2024-08-12 11:54:24 +0800

    1

--- Hello.cs
+++ Hello.cs
@@ -5,11 +5,14 @@ public class Hello
     public static void SayHello()
     {
         Debug.Log("Hello World!!!!!!!!!");
+        Debug.Log("Hello World!!!!!!!!!");
+        Debug.Log("Hello World!!!!!!!!!");
+        Debug.Log("Hello World!!!!!!!!!");
     }
 
     public static void TestType<T>(T obj)
     {
-        if (obj != null) Debug.Log(obj);
+        if (obj != null) Debug.Log(obj);   
     }
 
     public static void TestAddComponent()
