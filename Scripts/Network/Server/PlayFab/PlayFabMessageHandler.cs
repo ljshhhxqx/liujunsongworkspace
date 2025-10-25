250dcbfe03ca4138c9cb9cfccf809266cdb87269
DESKTOP-DU1KDKN\LxJqS <sserljs@qq.com>
2025-10-25 21:18:54 +0800

    1

--- Scripts/Network/Server/PlayFab/PlayFabMessageHandler.cs
+++ Scripts/Network/Server/PlayFab/PlayFabMessageHandler.cs
@@ -161,7 +161,9 @@ namespace Network.Server.PlayFab
                     Debug.Log($"Received 0 new messages");
                     return;
                 }
+                Debug.Log($"Received {value} new messages");
                 var newMessages = JsonUtility.FromJson<GetNewMessagesResponse>(value.ToString());
+                Debug.Log($"Received {newMessages.messages.Length} new messages");
                 var messages = newMessages.messages;
                 Debug.Log($"Received {messages.Length} new messages");
                 ProcessMessages(messages);
